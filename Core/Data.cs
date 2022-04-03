381352c9301e921758b2d70dfed26aae86ae8c64
Daryl Graves <darylgraves0309@gmail.com>
2022-04-03 20:24:18 +0100

    Data Constructor

--- Core/Data.cs
+++ Core/Data.cs
@@ -1,9 +1,47 @@
-﻿namespace Core;
+﻿using System.IO;
+namespace Core;
+
 public class Data
 {
-    public string myGreeting { get; set; }
-    public Data(string greeting)
+    public string[] csv { get; }
+    public char delimiter { get; }
+    
+    // Constructor
+    public Data(string path, char delimiter)
     {
-        myGreeting = greeting;
+        string[] csvToValidate;
+        
+        // Load CSV
+        if (File.Exists(path))
+        {
+            csvToValidate = File.ReadAllLines(path);               
+        }
+        else
+        {
+            throw new FileNotFoundException($"{path} not found!");
+        }
+
+        // Validate CSV is valid
+        
+        // First check there is more than just a header
+        if (csvToValidate.Length > 1)
+        {
+            // Then check it includes the delimiter 
+            if (csvToValidate[0].Split(delimiter).Count() > 1)
+            {
+                // Accepted 
+                this.csv = csvToValidate;
+                this.delimiter = delimiter;
+            }
+            else
+            {
+                throw new Exception("Invalid Csv File - Delimiter was not found");
+            }
+        }
+        else
+        {
+            throw new Exception("Invalid Csv File - File was less than 2 lines");
+        }
     }
+
 }
--- nCore/nData.cs
+++ nCore/nData.cs
@@ -5,16 +5,68 @@ namespace nCore;
 
 public class Tests
 {
+    string csv_1_noData = "../../../../Debug/Csvs/1_emptyFile.csv"; 
+    string csv_2_OnlyHeaders = "../../../../Debug/Csvs/2_onlyHeaders.csv"; 
+    string csv_3_OneRowOfData = "../../../../Debug/Csvs/3_oneRowOfData.csv";
+    
     [SetUp]
     public void Setup()
     {
     }
 
     [Test]
-    public void Test1()
+    public void ctor_1_EmptyFile()
     {
-        var myData = new Data("Hello, world!");
-        Assert.AreEqual("Hello, world", myData.myGreeting);
-        Assert.Pass();
+        bool dataNotLoaded = true;
+        try
+        {
+            var myData = new Data(csv_1_noData, ',');
+            dataNotLoaded = true;    
+        }
+        catch {}
+        
+        Assert.IsTrue(dataNotLoaded);
+    }
+    
+    [Test]
+    public void ctor_2_OnlyHeaders()
+    {
+        bool dataNotLoaded = true;
+        try
+        {
+            var myData = new Data(csv_2_OnlyHeaders, ',');
+            dataNotLoaded = true;    
+        }
+        catch {}
+        
+        Assert.IsTrue(dataNotLoaded);
+    }
+
+    [Test]
+    public void ctor_3_OneRowOfData_InvalidDelimiter()
+    {
+        bool dataNotLoaded = true;
+        try
+        {
+            var myData = new Data(csv_3_OneRowOfData, ';');
+            dataNotLoaded = true;    
+        }
+        catch {}
+        
+        Assert.IsTrue(dataNotLoaded);
+    }
+
+    [Test]
+    public void ctor_3_OneRowOfData_ValidDelimiter()
+    {
+        bool dataLoaded = false;
+        try
+        {
+            var myData = new Data(csv_3_OneRowOfData, ',');
+            dataLoaded = true;    
+        }
+        catch {}
+        
+        Assert.IsTrue(dataLoaded);
     }
 }
