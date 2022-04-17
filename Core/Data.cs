a0e1e167a11989979d4dd36685cd2b36a733d146
Daryl Graves <darylgraves0309@gmail.com>
2022-04-17 13:55:46 +0100

    Data - Updating ToDo Comments

--- Core/Data.cs
+++ Core/Data.cs
@@ -5,15 +5,20 @@ namespace Core;
 
 public class Data
 {
-    //TODO: Change most of these to Read-Only and use private variables for changes inside the class
+    //TODO: Data.Fields - Change to Read-Only where poss and use private variables for changes inside the class
     private string[] csv;
-    private char Delimiter;
-    private string[] Headers; 
+    private char delimiter;
+    private string[] headers; 
     private Stack<string>? KeywordsToDo;
     private Stack<string>? KeywordsCompleted;
     
+    //TODO: Data.Dictionary should be private but needs untangling because it will break Unit Tests
     public Dictionary<string, List<string>> WordRows { get; private set; } 
+
+    //TODO: Data.Keywords should be private but needs untangling because it will break Unit Tests
     public List<string> Keywords {get; private set;}
+
+    //TODO: Data.KeywordHeader should be private but needs untangling because it will break Unit Tests
     public string? KeywordHeader { get; private set; }
 
     // Constructor
@@ -36,7 +41,6 @@ public class Data
         // First check there is more than just a header
         if (csvToValidate.Length > 1)
         {
-            // TODO: Are we checking that each row has the same number of columns?
             var rowCount = csvToValidate[0].Split(Delimiter).Count();
 
             for (int i = 0; i < csvToValidate.Length; i++)
@@ -53,11 +57,11 @@ public class Data
             {
                 // This File was accepted 
                 this.csv = csvToValidate;
-                this.Delimiter = Delimiter;
+                this.delimiter = Delimiter;
 
 
                 this.Keywords = new List<string>();
-                this.Headers = csvToValidate[0].Split(Delimiter);
+                this.headers = csvToValidate[0].Split(Delimiter);
                 this.WordRows = new Dictionary<string, List<string>>();
             }
             else
@@ -76,7 +80,7 @@ public class Data
     ///</summary>
     public string[] GetHeaders()
     {
-        return Headers;
+        return headers;
     }
 
     ///<summary>
@@ -84,9 +88,9 @@ public class Data
     ///</summary>
     public int FindColumnNo(string headerToFind)
     {
-        for (int i = 0; i < Headers.Length; i++)
+        for (int i = 0; i < headers.Length; i++)
         {
-            if (Headers[i] == headerToFind)
+            if (headers[i] == headerToFind)
             {
                 return i;
             }
@@ -101,24 +105,22 @@ public class Data
     ///</summary>
     public void CreateCollection(int columnNo)
     {
-        //TODO: CreateCollection() we could make an override which takes the KeywordHeader directly so no faffing around with column numbers.
-
         if (csv == null)
         {
            return;
         }
 
         // Get the Header 
-        KeywordHeader = csv[0].Split(Delimiter)[columnNo];
+        KeywordHeader = csv[0].Split(delimiter)[columnNo];
 
         // Construct the Dictionary
         foreach (var row in csv)
         {
-            var keyword = row.Split(Delimiter)[columnNo];
+            var keyword = row.Split(delimiter)[columnNo];
             Keywords.Add(keyword);
 
             List<string> elements = new List<string>();
-            foreach (var element in row.Split(Delimiter))
+            foreach (var element in row.Split(delimiter))
             {
                 if (element != keyword)
                 {
@@ -139,7 +141,6 @@ public class Data
     ///</summary>
     public int AddColumn(string columnName)
     {
-        //TODO: Data.AddColumn() - Should add null entries manually for every row
         if(KeywordHeader == null)
         {
             // CreateCollection() hasn't been ran yet
@@ -224,7 +225,7 @@ public class Data
                 {
                     if (i != WordRows[keyword].Count -1)
                     {
-                        rows.Append(WordRows[keyword][i] + Delimiter);
+                        rows.Append(WordRows[keyword][i] + delimiter);
                     }
                     else
                     {
@@ -232,7 +233,7 @@ public class Data
                     }
                 }
 
-                string rowAsString = keyword + Delimiter + rows + '\n';
+                string rowAsString = keyword + delimiter + rows + '\n';
                 destinationFile.Write(rowAsString);
             }          
 
