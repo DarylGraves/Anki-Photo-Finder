fb9779a42613fb9357555e6e322458cfc583bc42
Daryl Graves <d.graves88@outlook.com>
2022-05-30 09:24:32 +0100

    Controller.Cs Added

---
+++ Core/Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public static class Controller
+    {
+        public static Data? Data { get; set; }
+
+        public static void LoadData(string path, char delimiter)
+        {
+            if (File.Exists(path))
+            {
+                Data = new Data(path, delimiter);
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+        }
+    }
+}
